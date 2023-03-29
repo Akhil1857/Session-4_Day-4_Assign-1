354515b06144de1a381e84a208019e32e9f7e024
Akhil1857 <125340453+Akhil1857@users.noreply.github.com>
2023-03-29 23:02:46 +0530

    Update ListManipulation.scala

--- Capitalize/src/main/scala/ListManipulation.scala
+++ Capitalize/src/main/scala/ListManipulation.scala
@@ -1,6 +1,8 @@
 import scala.util.{Failure, Success, Try}
 
 class ListManipulation {
+  
+  //This capitalAllCharacterA Function convert all 'a' to 'A'
   def capitalAllCharacterA(statement: List[String]): List[String] = {
     statement.map { string =>
       Try {
